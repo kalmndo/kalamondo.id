27d9b256381a6b4e5db0d857a7fd451900d2f33e
kalmndo <kalmndo@gmail.com>
2023-03-26 00:13:03 +0700

    add instagram icon

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,5 @@
+import { ArrowIcon, GitHubIcon, InstagramIcon, TwitterIcon } from "components/icons";
+
 export default function AboutPage() {
   return (
     <section>
@@ -24,6 +26,44 @@ export default function AboutPage() {
           to bring a unique perspective to my work and continuously challenge
           myself to expand my knowledge.
         </p>
+        <div className="flex flex-col gap-1 md:flex-row md:gap-2">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://twitter.com/leeerob"
+            className="flex w-full border border-neutral-200 dark:border-neutral-800 rounded-lg p-4 no-underline items-center text-neutral-800 dark:text-neutral-200 hover:dark:bg-neutral-900 hover:bg-neutral-100 transition-all justify-between"
+          >
+            <div className="flex items-center">
+              <TwitterIcon />
+              <div className="ml-3">Twitter</div>
+            </div>
+            <ArrowIcon />
+          </a>
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/leerob"
+            className="flex w-full border border-neutral-200 dark:border-neutral-800 rounded-lg p-4 no-underline items-center text-neutral-800 dark:text-neutral-200 hover:dark:bg-neutral-900 hover:bg-neutral-100 transition-all justify-between"
+          >
+            <div className="flex items-center">
+              <GitHubIcon />
+              <div className="ml-3">GitHub</div>
+            </div>
+            <ArrowIcon />
+          </a>
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.youtube.com/@leerob"
+            className="flex w-full border border-neutral-200 dark:border-neutral-800 rounded-lg p-4 no-underline items-center text-neutral-800 dark:text-neutral-200 hover:dark:bg-neutral-900 hover:bg-neutral-100 transition-all justify-between"
+          >
+            <div className="flex items-center">
+              <InstagramIcon />
+              <div className="ml-3">YouTube</div>
+            </div>
+            <ArrowIcon />
+          </a>
+        </div>
       </div>
     </section>
   );

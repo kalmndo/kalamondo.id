0db3491e5dd67358a6b4dad0a994ed88402a2eb0
Adam Kalamondo <kalmndo@gmail.com>
2024-08-06 10:00:12 +0700

    side bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,16 +72,15 @@ export default function RootLayout({
       )}
     >
       <body className="antialiased max-w-4xl mb-40 flex flex-col md:flex-row mx-4 mt-8 md:mt-20 lg:mt-32 lg:mx-auto">
-        <Sidebar />
+        {/* <Sidebar />
         <main className="flex-auto min-w-0 mt-6 md:mt-0 flex flex-col px-2 md:px-0">
           {children}
-          {/* <Analytics /> */}
         </main>
         <Script
           src="https://analytics-umami.vercel.app/script.js"
           data-website-id="46191a75-5b73-46dc-ae25-5531a2aff767"
           strategy="lazyOnload"
-        />
+        /> */}
       </body>
     </html>
   );
